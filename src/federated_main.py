fa33a936c3d2eeea033676881929418db3466485
Juan <zhaojuanamy@hust.edu.cn>
2020-12-03 23:51:59 +0800

    update fed_main to remove the useless traineval

--- src/federated_main.py
+++ src/federated_main.py
@@ -99,28 +99,28 @@ if __name__ == '__main__':
         loss_avg = sum(local_losses) / len(local_losses)
         train_loss.append(loss_avg)
 
-        # Calculate avg training accuracy over all users at every epoch
-        list_acc, list_loss = [], []
-        global_model.eval()
-        for c in range(args.num_users):
-            local_model = LocalUpdate(args=args, dataset=train_dataset,
-                                      idxs=user_groups[idx], logger=logger)
-            acc, loss = local_model.inference(model=global_model)
-            list_acc.append(acc)
-            list_loss.append(loss)
-        train_accuracy.append(sum(list_acc)/len(list_acc))
+        ## Calculate avg training accuracy over all users at every epoch
+        #list_acc, list_loss = [], []
+        #global_model.eval()
+        #for c in range(args.num_users):
+        #    local_model = LocalUpdate(args=args, dataset=train_dataset,
+        #                              idxs=user_groups[idx], logger=logger)
+        #    acc, loss = local_model.inference(model=global_model)
+        #    list_acc.append(acc)
+        #    list_loss.append(loss)
+        #train_accuracy.append(sum(list_acc)/len(list_acc))
 
-        # print global training loss after every 'i' rounds
-        if (epoch+1) % print_every == 0:
-            print(f' \nAvg Training Stats after {epoch+1} global rounds:')
-            print(f'Training Loss : {np.mean(np.array(train_loss))}')
-            print('Train Accuracy: {:.2f}% \n'.format(100*train_accuracy[-1]))
+        ## print global training loss after every 'i' rounds
+        #if (epoch+1) % print_every == 0:
+        #    print(f' \nAvg Training Stats after {epoch+1} global rounds:')
+        #    print(f'Training Loss : {np.mean(np.array(train_loss))}')
+        #    print('Train Accuracy: {:.2f}% \n'.format(100*train_accuracy[-1]))
 
     # Test inference after completion of training
     test_acc, test_loss = test_inference(args, global_model, test_dataset)
 
     print(f' \n Results after {args.epochs} global rounds of training:')
-    print("|---- Avg Train Accuracy: {:.2f}%".format(100*train_accuracy[-1]))
+    #print("|---- Avg Train Accuracy: {:.2f}%".format(100*train_accuracy[-1]))
     print("|---- Test Accuracy: {:.2f}%".format(100*test_acc))
 
     # Saving the objects train_loss and train_accuracy:
